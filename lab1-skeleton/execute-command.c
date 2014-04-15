d660943425c55bfb56cee73def72d8ba729501e6
rchen93 <rchen93@ucla.edu>
2014-04-14 22:52:25 -0700

    Laid down groundwork for other cases for execution

--- lab1-skeleton/execute-command.c
+++ lab1-skeleton/execute-command.c
@@ -8,8 +8,8 @@
 #include <string.h>
 #include <stdlib.h>
 #include <ctype.h>
-#include <fnctl.h>		// open file
-#include <unistd.h>		// dup2
+#include <fnctl.h>    // open file
+#include <unistd.h>   // dup2
 
 /* FIXME: You may need to add #include directives, macro definitions,
    static function definitions, etc.  */
@@ -23,49 +23,82 @@ command_status (command_t c)
 void
 execute_command (command_t c, bool time_travel)
 {
-  int fd[2];	// For PIPE
+  int fd[2];  // For PIPE
+
+  /* NOT SURE IF NEEDED */
+  int stdin2 = dup(0);
+  int stdout2 = dup(1);
 
   switch (c->type)
   {
-  	case SIMPLE_COMMAND:
-  	{
-  		int status;
-  		pid_t pid = fork();
+	case SIMPLE_COMMAND:
+	{
+	  int status;
+	  pid_t pid = fork();
 
-  		if (pid > 0)
-  			exit(1);
-  		else
-  		{
-  			if (c->input != NULL)
-  			{
-  				int inputRedir = open(c->input, O_RDONLY);
-  				if (inputRedir < 0)
-  					exit(1);
-  				if (dup2(inputRedir, 0) < 0)
-  					exit(1);
-  			}
-  			if (c->output != NULL)
-  			{
-  				int outputRedir = open(c->output, O_WRONLY);
-  				if (outputRedir < 0)
-  					exit(1);
-  				if (dup2(outputRedir, 1) < 0)
-  					exit(1);
-  			}
+	  if (pid > 0)
+		exit(1);
+	  else
+	  {
+		if (c->input != NULL)
+		{
+		  int inputRedir = open(c->input, O_RDONLY);
+		  if (inputRedir < 0)
+			exit(1);
+		  if (dup2(inputRedir, 0) < 0)
+			exit(1);
+		}
+		if (c->output != NULL)
+		{
+		  int outputRedir = open(c->output, O_WRONLY);
+		  if (outputRedir < 0)
+			exit(1);
+		  if (dup2(outputRedir, 1) < 0)
+			exit(1);
+		}
 
-  			char **split = str_split(*c->u.word);
+		char **split = str_split(*c->u.word);
 
-  			if (pid == 0)
-  				execvp(split[0], split);
-  			else
-  				waitpid(pid, &status, 0);
+		if (pid == 0)
+		  execvp(split[0], split);
+		else
+		  waitpid(pid, &status, 0);
 
-  			c->status = status;
-  			break;
-  		}
+		c->status = status;
+		break;
+	  }
+	}
 
-  		break;
-  	}
+	case AND_COMMAND: 
+	{
+		command_t left = c->u.command[0];
+		command_t right = c->u.command[1];
+		execute_command(left, time_travel);
+		c->status = left->status;
+
+		if (left->status == 0)
+		{
+			execute_command(right, time_travel);
+			c->status = right->status;
+		}
+		break;
+	}
+
+	// OR_COMMAND
+	// PIPE_COMMAND
+	// SUBSHELL_COMMAND
+	// SEQUENCE_COMMAND
+
+	default: return;
   }
+
+  // Clear file I/O
+
+  dup2(stdin2, 0);
+  dup2(stdout2, 1);
+  close(stdin2);
+  close(stdout2);
+
+
   
 }
