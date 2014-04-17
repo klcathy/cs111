9266581489878b28c14fd6e6c2caa0cfb4fe3427
rchen93 <rchen93@ucla.edu>
2014-04-17 00:18:53 -0700

    Modularized and added PIPE code
    echo Hello does not work luls

--- lab1-skeleton/execute-command.c
+++ lab1-skeleton/execute-command.c
@@ -8,11 +8,20 @@
 #include <string.h>
 #include <stdlib.h>
 #include <ctype.h>
-#include <fnctl.h>    // open file
+#include <fcntl.h>    // open file
 #include <unistd.h>   // dup2
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <errno.h>
 
-/* FIXME: You may need to add #include directives, macro definitions,
-   static function definitions, etc.  */
+void executingSimple(command_t c);
+void executingSubshell(command_t c);
+void executingAnd(command_t c);
+void executingOr(command_t c);
+void executingSequence(command_t c);
+void executingPipe(command_t c);
+char** split_string(char* str);
 
 int
 command_status (command_t c)
@@ -20,85 +29,213 @@ command_status (command_t c)
   return c->status;
 }
 
-void
-execute_command (command_t c, bool time_travel)
+char** split_string(char* str)
 {
-  int fd[2];  // For PIPE
-
-  /* NOT SURE IF NEEDED */
-  int stdin2 = dup(0);
-  int stdout2 = dup(1);
-
-  switch (c->type)
-  {
-	case SIMPLE_COMMAND:
+	int num = 0; // Number of strings split
+	char* s;
+	char** result = (char**)checked_malloc(sizeof(char*));
+	s = strtok (str," "); //Tokenizes each string component
+	while (s != NULL)
 	{
-	  int status;
-	  pid_t pid = fork();
+		result = checked_realloc(result, (num+2)*sizeof(char*)); // 1 for string, 1 for NULL
+		result[num] = s;
+		num++; 
+		result[num] = NULL; 
+		s = strtok (NULL, " ");
+	}
 
-	  if (pid > 0)
+	return result;
+}
+
+void execute_switch (command_t c)
+{
+	switch (c->type)
+	{
+		case SIMPLE_COMMAND:
+			executingSimple(c);
+			break;
+		case SUBSHELL_COMMAND:
+			executingSubshell(c);
+			break;
+		case AND_COMMAND:
+			executingAnd(c);
+			break;
+		case OR_COMMAND:
+			executingOr(c);
+			break;
+		case SEQUENCE_COMMAND:
+			executingSequence(c);
+			break;
+		case PIPE_COMMAND:
+			executingPipe(c);
+			break;
+		default:
+			error(1, 0, "Not a valid command");
+	}
+}
+
+void executingSimple(command_t c)
+{
+	int status;
+	pid_t pid = fork();
+
+	if (pid < 0)
+	{
+		error (1, errno, "fork was unsuccesful\n");
+	}
+
+	if (pid == 1)
+	{
+		printf("Parent\n");
 		exit(1);
-	  else
-	  {
+	}
+	else
+	{
 		if (c->input != NULL)
 		{
-		  int inputRedir = open(c->input, O_RDONLY);
-		  if (inputRedir < 0)
-			exit(1);
-		  if (dup2(inputRedir, 0) < 0)
-			exit(1);
-		}
-		if (c->output != NULL)
-		{
-		  int outputRedir = open(c->output, O_WRONLY);
-		  if (outputRedir < 0)
-			exit(1);
-		  if (dup2(outputRedir, 1) < 0)
-			exit(1);
+			int inputRedir = open(c->input, O_RDONLY | O_CREAT, 0644);
+			if (inputRedir < 0)
+				exit(1);
+			if (dup2(inputRedir, 0) < 0)
+				exit(1);
 		}
 
-		char **split = str_split(*c->u.word);
+		if (c->output != NULL)
+		{
+			int outputRedir = open(c->output, O_WRONLY | O_TRUNC| O_CREAT, 0644);
+		  	if (outputRedir < 0)
+				exit(1);
+		  	if (dup2(outputRedir, 1) < 0)
+				exit(1);
+		}
+
+		char **split = split_string(*c->u.word);
 
 		if (pid == 0)
 		  execvp(split[0], split);
 		else
 		  waitpid(pid, &status, 0);
 
-		c->status = status;
-		break;
-	  }
+		c->status = WEXITSTATUS(status);
+		return;
 	}
+}
 
-	case AND_COMMAND: 
+void executingAnd(command_t c)
+{
+	command_t left = c->u.command[0];
+	command_t right = c->u.command[1];
+	execute_command(left, false);
+	c->status = WEXITSTATUS(left->status);
+
+	if (left->status == 0)
 	{
-		command_t left = c->u.command[0];
-		command_t right = c->u.command[1];
-		execute_command(left, time_travel);
-		c->status = left->status;
+		execute_command(right, false);
+		c->status = WEXITSTATUS(right->status);
+	}
+	return;
+}
 
-		if (left->status == 0)
-		{
-			execute_command(right, time_travel);
-			c->status = right->status;
-		}
-		break;
+void executingSubshell(command_t c)
+{
+	return;
+}
+
+void executingOr(command_t c)
+{
+	return;
+}
+
+void executingSequence(command_t c)
+{
+	return;
+}
+
+void executingPipe(command_t c)
+{
+	pid_t returnedPid;
+	pid_t firstPid;
+	pid_t secondPid;
+	int buffer[2];
+	int eStatus;
+
+	if ( pipe(buffer) < 0 )
+	{
+		error (1, errno, "pipe was not created");
 	}
 
-	// OR_COMMAND
-	// PIPE_COMMAND
-	// SUBSHELL_COMMAND
-	// SEQUENCE_COMMAND
+	firstPid = fork();
+	if (firstPid < 0)
+    {
+		error(1, errno, "fork was unsuccessful");
+    }
+	else if (firstPid == 0) //child executes command on the right of the pipe
+	{
+		close(buffer[1]); //close unused write end
 
-	default: return;
-  }
+        //redirect standard input to the read end of the pipe
+        //so that input of the command (on the right of the pipe)
+        //comes from the pipe
+		if ( dup2(buffer[0], 0) < 0 )
+		{
+			error(1, errno, "error with dup2");
+		}
+		execute_switch(c->u.command[1]);
+		_exit(c->u.command[1]->status);
+	}
+	else 
+	{
+		// Parent process
+		secondPid = fork(); //fork another child process
+                            //have that child process executes command on the left of the pipe
+		if (secondPid < 0)
+		{
+			error(1, 0, "fork was unsuccessful");
+		}
+        else if (secondPid == 0)
+		{
+			close(buffer[0]); //close unused read end
+			if(dup2(buffer[1], 1) < 0) //redirect standard output to write end of the pipe
+            {
+				error (1, errno, "error with dup2");
+            }
+			execute_switch(c->u.command[0]);
+			_exit(c->u.command[0]->status);
+		}
+		else
+		{
+			// Finishing processes
+			returnedPid = waitpid(-1, &eStatus, 0); //this is equivalent to wait(&eStatus);
+                        //we now have 2 children. This waitpid will suspend the execution of
+                        //the calling process until one of its children terminates
+                        //(the other may not terminate yet)
 
-  // Clear file I/O
+			//Close pipe
+			close(buffer[0]);
+			close(buffer[1]);
 
-  dup2(stdin2, 0);
-  dup2(stdout2, 1);
-  close(stdin2);
-  close(stdout2);
+			if (secondPid == returnedPid )
+			{
+			    //wait for the remaining child process to terminate
+				waitpid(firstPid, &eStatus, 0); 
+				c->status = WEXITSTATUS(eStatus);
+				return;
+			}
+			
+			if (firstPid == returnedPid)
+			{
+			    //wait for the remaining child process to terminate
+   				waitpid(secondPid, &eStatus, 0);
+				c->status = WEXITSTATUS(eStatus);
+				return;
+			}
+		}
+	}	
+}
 
-
-  
+void
+execute_command (command_t c, bool time_travel)
+{
+ 	if (time_travel == false)
+ 		execute_switch(c);
 }
